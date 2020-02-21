4f488183de51e99e0859096d07c6370299029061
JasmineLau <55225223+AppleTater@users.noreply.github.com>
2020-02-20 19:28:37 -0800

    Create IntVector.h

---
+++ PROGRAMS/4. IntVector Class/IntVector.h
@@ -0,0 +1,39 @@
+#ifndef _INTVECTOR_H_
+#define _INTVECTOR_H_
+#include <iostream>
+
+using namespace std;
+
+class IntVector {
+    private:
+        unsigned sz; //stores the size of the IntVector (the number of elements currently being used).
+        unsigned cap; //store the size of the array
+        int *data; //stores the address of the dynamically-allocated array of integers
+        void expand();
+        void expand(unsigned int x);
+    
+    public:
+        IntVector();
+        IntVector(unsigned size, int value = 0);
+        ~IntVector();
+        unsigned size() const;
+        unsigned capacity() const;
+        bool empty() const;
+        const int & at(unsigned index) const;
+        int & at(unsigned index);
+        void insert(unsigned index, int value);
+        void erase(unsigned index);
+        const int & front() const;
+        int & front();
+        const int & back() const;
+        int & back();
+        void assign(unsigned n, int value);
+        void push_back(int value);
+        void pop_back();
+        void clear();
+        void resize(unsigned int size, int value = 0);
+        void reserve(unsigned n);
+
+};
+
+#endif
